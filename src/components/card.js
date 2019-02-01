a69d2a73684ea404e8acb7e79483edc13d9bc058
Evan Guirino <etg624@gmail.com>
2019-01-31 17:46:10 -0800

    made main card component switching to see if grid is easier to style w/

---
+++ src/components/card.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './card.css';
+
+function JobCard() {
+  return (
+    <div className="job-card">
+      <section className="job-desc">
+        <h2 className="job-title">Job 1</h2>
+        <p className="job-details">
+          Software engineer for a pretty cool company
+        </p>
+        <div className="location-pay">
+          <p>Los Angeles</p>
+          <p>$25-$30 an hr</p>
+        </div>
+      </section>
+      <form className="job-form">
+        <textarea
+          className="job-notes"
+          placeholder="Some notes about the job"
+        />
+        <button className="schedule-button" type="submit">
+          Schedule
+        </button>
+        <span className="checkbox">
+          <input type="checkbox" />
+          Applied?
+        </span>
+      </form>
+    </div>
+  );
+}
+
+export default JobCard;
