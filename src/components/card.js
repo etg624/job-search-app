44a498ba54e3baad530ef1ed82eb57286d1ed9a6
Evan Guirino <etg624@gmail.com>
2019-02-01 13:44:53 -0800

    trying css/grid

--- src/components/card.js
+++ src/components/card.js
@@ -4,29 +4,34 @@ import './card.css';
 function JobCard() {
   return (
     <div className="job-card">
-      <section className="job-desc">
-        <h2 className="job-title">Job 1</h2>
-        <p className="job-details">
-          Software engineer for a pretty cool company
-        </p>
-        <div className="location-pay">
-          <p>Los Angeles</p>
-          <p>$25-$30 an hr</p>
-        </div>
-      </section>
-      <form className="job-form">
-        <textarea
-          className="job-notes"
-          placeholder="Some notes about the job"
-        />
-        <button className="schedule-button" type="submit">
-          Schedule
-        </button>
-        <span className="checkbox">
-          <input type="checkbox" />
-          Applied?
-        </span>
-      </form>
+      <div className="card-container">
+        <section className="job-desc">
+          <h2 className="job-title">Job 1</h2>
+          <p className="job-details">
+            Software engineer for a pretty cool company
+          </p>
+          <div className="location-pay">
+            <p>Los Angeles</p>
+            <p>$25-$30 an hr</p>
+          </div>
+        </section>
+        <form className="job-form">
+          <textarea
+            className="job-notes"
+            placeholder="Some notes about the job"
+          />
+          <button className="schedule-button" type="submit">
+            Schedule
+          </button>
+          <button className="schedule-button" type="submit">
+            Save Note
+          </button>
+          <span className="checkbox">
+            <input type="checkbox" />
+            Applied?
+          </span>
+        </form>
+      </div>
     </div>
   );
 }
